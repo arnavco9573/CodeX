77d35e2affc46fdd75ebfb2562ccacba5d5d401d
arnavco9573 <arnavsharm589@gmail.com>
2025-09-08 23:36:02 +0530

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-
-export default function Home() {
+export default async function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "From Trpc" }))
+  console.log("Server")
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     Heloo world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading....</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
---
+++ src/config/envConfig.ts
@@ -0,0 +1,2 @@
+export const DATABASE_URL = process.env.DATABASE_URL
+export const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL
