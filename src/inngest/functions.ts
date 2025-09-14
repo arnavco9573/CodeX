f9176032ea87d113de926b411dc5744c73d8b55c
arnavco9573 <arnavsharm589@gmail.com>
2025-09-14 18:59:34 +0530

    AI_JOBS

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from "./client";
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "writer",
+      system: "You are an expert summarizer. You summarize in 2 words",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.value}`
+    );
+
+    console.log(output);
+    return { output };
+  }
 );
