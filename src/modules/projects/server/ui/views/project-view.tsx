e8a9c5ab07580e4885dd9bb44a2084a2a8e68805
arnavco9573 <arnavsharm589@gmail.com>
2025-10-22 17:41:40 +0530

    project-header

--- src/modules/projects/server/ui/views/project-view.tsx
+++ src/modules/projects/server/ui/views/project-view.tsx
@@ -5,13 +5,17 @@ import { useTRPC } from "@/trpc/client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 import MessagesContainer from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
     projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+
     return (
         <div className="h-screen ">
             <ResizablePanelGroup direction="horizontal" >
@@ -19,8 +23,14 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={35}
                     minSize={20}
                     className="flex flex-col min-h-0">
+                    <Suspense fallback={<div>Loading Project...</div>} >
+                        <ProjectHeader projectId={projectId} />
+                    </Suspense>
                     <Suspense fallback={<div>Loading...</div>}>
-                        <MessagesContainer projectId={projectId} />
+                        <MessagesContainer
+                            projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment} />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
